c14a7592e3b7d1005a4b6b0fc0a60bd294edb778
victormoraes <victor.moraesrj@gmail.com>
2024-08-01 22:27:58 -0300

    Task View, Interface using colorama

--- src/task_view.py
+++ src/task_view.py
@@ -1,6 +1,33 @@
+from colorama import Fore, Style 
+
+
 class TaskView:
     def prompt_for_task_id(self):
         title = input("Digite o título da Tarefa: ")
-        description = input(f"Digite uma descrição para '{title}': (Opcional) ")
+        description = input(f"Digite uma descrição para Tarefa: (Opcional) ")
+        deadline = input(f"Digite um prazo para Tarefa: (AAAA-MM-DD) ")
+        urgency_level = input(f"Digite uma descrição para Tarefa: (Opcional) ")
         
+        return title, description, deadline, urgency_level
+    
+    def display_tasks(self, tasks):
+        print("Tarefas:")
+        for task in tasks:
+            print(f"ID: {task.id}, Titulo: {task.title}, Status: {task.status}")
+            
+    def display_full_task(self, task):
+        print(f"ID: {task.id}, Titulo: {task.title}, Status: {task.status}")
+        print(f"Data de Criação: {task.created_at} | Prazo: {task.deadline} | Urgência: {task.urgency_level}")
+        print(f"Descrição: {task.description}")
+    
+    def display_task_not_found(self, task_id):
+        print(f"Tarefa com ID {task_id} não pode ser encontrada :(")
         
+    def show_menu(self):
+        print("\n" + "=" * 20 + " Menu " + "=" * 20)
+        print(f"{Fore.GREEN}1. Adicionar Tarefa{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}2. Listar Tarefas{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}3. Marcar Tarefa como Concluída{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}4. Remover Tarefa{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}5. Sair{Style.RESET_ALL}")
+        return input(f"{Fore.YELLOW}Escolha uma opção: {Style.RESET_ALL}")
---
+++ tests/test_task_view.py
@@ -0,0 +1,8 @@
+import unittest
+from src.task_view import TaskView
+
+class TestTaskView(unittest.TestCase):
+    def test_display_tasks(self):
+        tv = TaskView()
+        tv.show_menu()
+        pass
